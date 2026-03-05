11d9a96f5a7f0405d3541d7853eea8d2e8522e93
sadcat21 <mycompt4tkallaoui@gmail.com>
2026-03-05 02:32:04 +0000

    Build APK / updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.5df8337765cf479a8b00f49289abc707',
+  appId: 'com.laserfood.app',
   appName: 'Laser Food',
   webDir: 'dist',
   server: {
-    url: 'https://dispro60.vercel.app',
+    url: 'https://dispro60.vercel.app/',
     cleartext: true
   }
 };
