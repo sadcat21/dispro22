d00056aa67333e5368a0c099291a89926146df57
kat4coder <101845122+kat4coder@users.noreply.github.com>
2026-03-27 06:09:41 +0100

    auto update 🚀 3/27/2026, 6:09:41 AM

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import RuntimeGuard from "./components/RuntimeGuard.tsx";
 import { ErrorBoundary } from "./components/ErrorBoundary.tsx";
 import App from "./App.tsx";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    updateSW(true);
+  },
+  onOfflineReady() {
+    console.info("App is ready for offline use.");
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <RuntimeGuard>
